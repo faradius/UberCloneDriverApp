e04f3309eb436752ef4576ea493c0063b178980d
Alejandro <faradius.magic187@gmail.com>
2022-12-08 15:57:28 -0600

    Integración de google maps

--- app/src/main/java/com/alex/uberclonedriverapp/activities/MapActivity.kt
+++ app/src/main/java/com/alex/uberclonedriverapp/activities/MapActivity.kt
@@ -4,13 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alex.uberclonedriverapp.R
 import com.alex.uberclonedriverapp.databinding.ActivityMapBinding
+import com.alex.uberclonedriverapp.utils.Config
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
     private lateinit var binding: ActivityMapBinding
+    private var googleMap:GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Config.setVersionCompatibilityStatusBar(window)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+    }
+
+
 }
