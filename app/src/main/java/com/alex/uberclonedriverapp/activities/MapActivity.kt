7d48657042acd0e7bed506d1c697cef23bf9d625
Alejandro <faradius.magic187@gmail.com>
2022-12-08 17:02:20 -0600

    Obteniendo ubicación en tiempo real

--- app/src/main/java/com/alex/uberclonedriverapp/activities/MapActivity.kt
+++ app/src/main/java/com/alex/uberclonedriverapp/activities/MapActivity.kt
@@ -1,18 +1,35 @@
 package com.alex.uberclonedriverapp.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import com.alex.uberclonedriverapp.R
 import com.alex.uberclonedriverapp.databinding.ActivityMapBinding
 import com.alex.uberclonedriverapp.utils.Config
+import com.example.easywaylocation.EasyWayLocation
+import com.example.easywaylocation.Listener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback,Listener {
+    private val TAG = "LOCALIZACIÓN"
 
     private lateinit var binding: ActivityMapBinding
     private var googleMap:GoogleMap? = null
+    private var easyWayLocation:EasyWayLocation? = null
+    private var myLocationLatLng: LatLng? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +39,81 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val locationRequest = LocationRequest.create().apply {
+            interval = 0
+            fastestInterval = 0
+            priority = Priority.PRIORITY_HIGH_ACCURACY
+            smallestDisplacement = 1f
+
+        }
+
+        easyWayLocation = EasyWayLocation(this, locationRequest, false, false,this)
+
+        locationPermission.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ))
+    }
+
+    val locationPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){ permission ->
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            when {
+                permission.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    Log.d(TAG, "Permiso concedido")
+                    easyWayLocation?.startLocation()
+                }
+                permission.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    Log.d(TAG, "Permiso concedido con limitación")
+                    easyWayLocation?.startLocation()
+                }
+                else -> {
+                    Log.d(TAG, "Permiso no concedido")
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        easyWayLocation?.startLocation()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        easyWayLocation?.endUpdates()
     }
 
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        googleMap?.isMyLocationEnabled = true
+    }
+
+    override fun locationOn() {
+
+    }
+
+    override fun currentLocation(location: Location) {
+        //Obteniendo la latitud y longitud de la posición actual
+        myLocationLatLng = LatLng(location.latitude, location.longitude)
+
+        googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition(
+            CameraPosition.builder().target(myLocationLatLng!!).zoom(17f).build()
+        ))
+    }
+
+    override fun locationCancelled() {
+
     }
 
 
