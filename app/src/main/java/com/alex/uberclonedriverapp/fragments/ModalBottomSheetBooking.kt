14cd7ece94fb5c21202e0e4ccadb1b47a85b4788
Alejandro <faradius.magic187@gmail.com>
2022-12-31 14:17:34 -0600

    Mostrando los datos de la solicitud en el Modal Bottom Sheet

--- app/src/main/java/com/alex/uberclonedriverapp/fragments/ModalBottomSheetBooking.kt
+++ app/src/main/java/com/alex/uberclonedriverapp/fragments/ModalBottomSheetBooking.kt
@@ -5,12 +5,21 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.alex.uberclonedriverapp.R
 import com.alex.uberclonedriverapp.models.Booking
 import com.alex.uberclonedriverapp.utils.Constants
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ModalBottomSheetBooking: BottomSheetDialogFragment() {
+
+    private lateinit var tvOrigin:TextView
+    private lateinit var tvDestination:TextView
+    private lateinit var tvTimeAndDistance: TextView
+    private lateinit var btnAccept: Button
+    private lateinit var btnCancel: Button
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,9 +27,19 @@ class ModalBottomSheetBooking: BottomSheetDialogFragment() {
     ):View?{
         val view = inflater.inflate(R.layout.modal_bottom_sheet_booking, container, false)
 
+        tvOrigin = view.findViewById(R.id.tvOrigin)
+        tvDestination = view.findViewById(R.id.tvDestination)
+        tvTimeAndDistance = view.findViewById(R.id.tvTimeAndDistance)
+        btnAccept = view.findViewById(R.id.btnAccept)
+        btnCancel = view.findViewById(R.id.btnCancel)
+
         val data = arguments?.getString(Constants.BOOKING)
         val booking = Booking.fromJson(data!!)
         Log.d("ARGUMENTS BOOKING", "Booking: ${booking?.toJson()} ")
+
+        tvOrigin.text = booking?.origin
+        tvDestination.text = booking?.destination
+        tvTimeAndDistance.text = "${booking?.time} Min - ${booking?.km} Km"
 
         return view
     }
